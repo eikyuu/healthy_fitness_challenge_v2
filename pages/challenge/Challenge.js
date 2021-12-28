bb9044a473b7c45ef3ae9184161ce4bbf5f76ae7
vduguet <vduguet@agencereference.com>
2021-12-28 18:52:11 +0100

    page challenge

--- pages/challenge/Challenge.js
+++ pages/challenge/Challenge.js
@@ -1,16 +1,77 @@
 import React from "react";
-import {Button, Text, View} from "react-native";
+import ImageBackground from "../../styles/global/ImageBackground";
+import Container from "../../styles/global/Container";
+import Title from "../../styles/global/Title";
+import Button from "../../styles/Home/Button";
+import {
+    View,
+    ScrollView,
+    StyleSheet
+} from 'react-native';
+import TitleChallenge from "../../styles/challenge/TitleChallenge";
 
 const Challenge = ({navigation}) => {
     return (
-        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-            <Text>Home Screen</Text>
-            <Button
-                title="Go to Challenge"
-                onPress={() => navigation.navigate('Challenge')}
-            />
-        </View>
+        <ImageBackground
+            source={require('../../assets/images/backgroundImage.jpg')} resizeMode="cover"
+        >
+            <ScrollView>
+                <Container>
+                    <View style={styles.container}>
+                        <TitleChallenge>
+                            Sélectionner un ou plusieurs exercices
+                        </TitleChallenge>
+                        <Button
+                            onPress={() => navigation.navigate('Mes challenges')}
+                        >
+                            <Title>Mes challenges</Title>
+                        </Button>
+                        <Button
+                            onPress={() => navigation.navigate('Mes challenges')}
+                        >
+                            <Title>Mes challenges</Title>
+                        </Button>
+                        <Button
+                            onPress={() => navigation.navigate('Mes challenges')}
+                        >
+                            <Title>Mes challenges</Title>
+                        </Button>
+                        <Button
+                            onPress={() => navigation.navigate('Mes challenges')}
+                        >
+                            <Title>Mes challenges</Title>
+                        </Button>
+                        <Button
+                            onPress={() => navigation.navigate('Mes challenges')}
+                        >
+                            <Title>Mes challenges</Title>
+                        </Button>
+                        <Button
+                            onPress={() => navigation.navigate('Mes challenges')}
+                        >
+                            <Title>Mes challenges</Title>
+                        </Button>
+                        <Button
+                            onPress={() => navigation.navigate('Mes challenges')}
+                        >
+                            <Title>Mes challenges</Title>
+                        </Button>
+                        <Button
+                            onPress={() => navigation.navigate('Mes challenges')}
+                        >
+                            <Title>Mes challenges</Title>
+                        </Button>
+                    </View>
+                </Container>
+            </ScrollView>
+        </ImageBackground>
     );
 }
-
+const styles = StyleSheet.create({
+    container: {
+        flex:1,
+        paddingTop: 10,
+        alignItems: "center",
+    },
+});
 export default Challenge;
---
+++ styles/challenge/TitleChallenge.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components/native';
+import Title from "../global/Title";
+
+const TitleChallenge = styled(Title)`
+  text-align: center;
+  margin-bottom: 10px;
+`;
+
+export default TitleChallenge;
