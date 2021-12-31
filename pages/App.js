cdd74c853e5691a79b47fb396652186ecfe7cc36
vduguet <vduguet@agencereference.com>
2021-12-31 10:00:59 +0100

    remove stackNav

--- pages/App.js
+++ pages/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import {NavigationContainer} from "@react-navigation/native";
 import BottomNav from "../components/navigation/BottomNav";
-import StackNav from "../components/navigation/StackNav";
 
 export default function App() {
     return (
